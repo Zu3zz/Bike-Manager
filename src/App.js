604dbea4a95eb97e22aab18583f91d2fdf770075
Zu3zz <zthzththth@163.com>
2019-07-09 00:52:55 +0800

    add less

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import './App2.less'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Appheader">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
