265705c3b7e71fc1b62d250c616aa8487522edf2
Zu3zz <zthzththth@163.com>
2019-07-09 23:44:37 +0800

    update resource

--- src/admin.js
+++ src/admin.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react'
 import { Row, Col  } from 'antd';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+import './style/common.less'
 
 class Admin extends Component{
 
   render(){
     return (
-      <Row>
-        <Col span={3}>left</Col>
-        <Col span={21}>
-          <Header></Header>
-          <Row>
+      <Row className="container">
+        <Col span={4} className="nav-left">
+          <NavLeft />
+        </Col>
+        <Col span={20} className="main">
+          <Header />
+          <Row className="content">
             Content
           </Row>
-          <Footer></Footer>
+          <Footer />
         </Col>
       </Row>
     )
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NavLeft extends Component{
+  render() {
+    return (
+      <div>
+        This is NavLeft
+      </div>
+    )
+  }
+}
+
+export default NavLeft
