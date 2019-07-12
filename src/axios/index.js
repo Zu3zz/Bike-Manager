75bd7c238c20e581f075ae070e145bf254220c9c
Zu3zz <zthzththth@163.com>
2019-07-13 00:08:06 +0800

    finish Header style

---
+++ src/axios/index.js
@@ -0,0 +1,20 @@
+import JsonP from 'jsonp'
+import 'axios'
+
+class Axios {
+  static jsonp(options) {
+    return new Promise((resolve, reject) => {
+      JsonP(options.url, {
+        param: 'callback'
+      }, function (err, response) {
+        if (response.status === 'success') {
+          resolve(response)
+        } else {
+          reject(response.message)
+        }
+      })
+    })
+  }
+}
+
+export default Axios
